c3bdad03ff613c39f1f312d883c07a71dafb3529
Ian Mooney <ianmooney@ian-mooneys-imac-6.local>
2010-10-14 15:31:27 +0100

    fixed magic_create template

--- generators/magic_list/magic_list_generator.rb
+++ generators/magic_list/magic_list_generator.rb
@@ -19,6 +19,7 @@ class MagicListGenerator < Rails::Generator::NamedBase
   def magic_create
     if request.xhr?
       render :update do |page|
+        @#{singular_name} = #{class_name}.new(params[:#{singular_name}])
         if @#{singular_name}.save
           page["magic_#{singular_name}_list"].append(render("#{plural_name}/magic_list_item", :#{singular_name} => @#{singular_name}))
           @#{singular_name} = #{class_name}.new
