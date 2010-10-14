61e3127fb5b356b8e207b7e9a5e7d8a99971dc23
Matt Atkins <matt@yoomee.com>
2010-10-14 15:26:30 +0100

    Insert action finished?

--- generators/insert_commands.rb
+++ generators/insert_commands.rb
@@ -8,21 +8,21 @@ end
 Rails::Generator::Commands::Destroy.class_eval do
   def insert(relative_destination, regexp, *args, &block)
     gsub_file(relative_destination, regexp, *args, &block)
-    logger.insert "Stuff"
+    logger.insert "magic_create action added to #{relative_destination}"
   end
 end
 
 Rails::Generator::Commands::Update.class_eval do
   def insert(relative_destination, regexp, *args, &block)
     gsub_file(relative_destination, regexp, *args, &block)
-    logger.insert "Stuff"
+    logger.insert "magic_create action added to #{relative_destination}"
   end
 end
 
 Rails::Generator::Commands::List.class_eval do
   def insert(relative_destination, regexp, *args, &block)
     gsub_file(relative_destination, regexp, *args, &block)
-    logger.insert "Stuff"
+    logger.insert "magic_create action added to #{relative_destination}"
   end
 end
 
--- generators/magic_list/magic_list_generator.rb
+++ generators/magic_list/magic_list_generator.rb
@@ -5,7 +5,7 @@ class MagicListGenerator < Rails::Generator::NamedBase
     record do |m|
       m.class_collisions "#{class_name}Controller", "#{class_name}ControllerTest", "#{class_name}Helper", "#{class_name}HelperTest"
       m.directory File.join('client/app/controllers')
-      m.insert "client/app/controllers/#{plural_name}_controller.rb", /^.*#{class_name.pluralize}Controller.*$/ {|match|"#{match}\n#{magic_create_action}"}
+      m.insert "client/app/controllers/#{plural_name}_controller.rb", /^.*#{class_name.pluralize}Controller.*$/ do |match|"#{match}\n#{magic_create_action}" end
       m.directory File.join('client/app/views/', plural_name)
       m.template('_magic_list.html.haml', "client/app/views/#{plural_name}/_magic_list.html.haml")
       m.template('_magic_list_item.html.haml', "client/app/views/#{plural_name}/_magic_list_item.html.haml")
@@ -16,17 +16,17 @@ class MagicListGenerator < Rails::Generator::NamedBase
   
   def magic_create_action
     <<-EOF
-def magic_create
-  if request.xhr?
-    render :update do |page|
-      if @#{singular_name}.save
-        page["magic_#{singular_name}_list"].append(render("#{plural_name}/magic_list_item", :#{singular_name} => @#{singular_name}))
-        @#{singular_name} = #{class_name}.new
+  def magic_create
+    if request.xhr?
+      render :update do |page|
+        if @#{singular_name}.save
+          page["magic_#{singular_name}_list"].append(render("#{plural_name}/magic_list_item", :#{singular_name} => @#{singular_name}))
+          @#{singular_name} = #{class_name}.new
+        end
+        page["magic_#{singular_name}_form"].replace_html(render("#{plural_name}/magic_form", :#{singular_name} => @#{singular_name}))
       end
-      page["magic_#{singular_name}_form"].replace_html(render("#{plural_name}/magic_form", :#{singular_name} => @#{singular_name}))
     end
   end
-end
     EOF
   end
 end
