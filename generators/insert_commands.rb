d450d2f86709d179d8b6950094e0f75ee8421f4d
Matt Atkins <matt@yoomee.com>
2010-10-14 15:23:33 +0100

    Fixed insert command.

--- generators/insert_commands.rb
+++ generators/insert_commands.rb
@@ -1,7 +1,7 @@
 Rails::Generator::Commands::Create.class_eval do
   def insert(relative_destination, regexp, *args, &block)
     gsub_file(relative_destination, regexp, *args, &block)
-    logger.insert "Stuff"
+    logger.insert "magic_create action added to #{relative_destination}"
   end
 end
 
@@ -27,7 +27,7 @@ Rails::Generator::Commands::List.class_eval do
 end
 
 def gsub_file(relative_destination, regexp, *args, &block)
-  path = destination_path(relative_destination)
+  path = relative_destination
   content = File.read(path).gsub(regexp, *args, &block)
   File.open(path, 'wb') { |file| file.write(content) }
 end
--- generators/magic_list/magic_list_generator.rb
+++ generators/magic_list/magic_list_generator.rb
@@ -3,23 +3,30 @@ require File.expand_path(File.dirname(__FILE__) + "/../insert_commands.rb")
 class MagicListGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      # Check for class naming collisions.
       m.class_collisions "#{class_name}Controller", "#{class_name}ControllerTest", "#{class_name}Helper", "#{class_name}HelperTest"
-      
-      
-      m.insert "client/app/controllers/#{plural_name}_controller.rb", /(#{Regexp.escape("^(.*<%=class_name%>sController.*$)")})/m do |match|
-        "#{match}\n  #{render :file => 'templates/magic_create_action.rb'}"
-      end
-            
       m.directory File.join('client/app/controllers')
-      m.template 'controller.rb', File.join('client/app/controllers',"#{file_name}_controller.rb")
-      
+      m.insert "client/app/controllers/#{plural_name}_controller.rb", /^.*#{class_name.pluralize}Controller.*$/ {|match|"#{match}\n#{magic_create_action}"}
       m.directory File.join('client/app/views/', plural_name)
       m.template('_magic_list.html.haml', "client/app/views/#{plural_name}/_magic_list.html.haml")
       m.template('_magic_list_item.html.haml', "client/app/views/#{plural_name}/_magic_list_item.html.haml")
       m.template('_magic_form.html.haml', "client/app/views/#{plural_name}/_magic_form.html.haml")
-      
       logger.readme("Example usage: =render(\"#{plural_name}/magic_list\", :#{plural_name} => @#{plural_name}, :new_#{singular_name} => #{class_name}.new)")
     end
+  end
+  
+  def magic_create_action
+    <<-EOF
+def magic_create
+  if request.xhr?
+    render :update do |page|
+      if @#{singular_name}.save
+        page["magic_#{singular_name}_list"].append(render("#{plural_name}/magic_list_item", :#{singular_name} => @#{singular_name}))
+        @#{singular_name} = #{class_name}.new
+      end
+      page["magic_#{singular_name}_form"].replace_html(render("#{plural_name}/magic_form", :#{singular_name} => @#{singular_name}))
+    end
+  end
+end
+    EOF
   end
 end
