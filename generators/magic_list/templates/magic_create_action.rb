b9adaa94d82c436421ff1bb2d802e4c13746741a
Ian Mooney <ianmooney@ian-mooneys-imac-6.local>
2010-10-14 16:31:30 +0100

    added destroy action and remote destroy link

--- generators/magic_list/templates/magic_create_action.rb
+++ generators/magic_list/templates/magic_create_action.rb
@@ -1,6 +1,7 @@
 def magic_create
   if request.xhr?
     render :update do |page|
+      @<%=singular_name%> = <%=class_name%>.new(params[:<%=singular_name%>])
       if @<%=singular_name%>.save
         page["magic_<%=singular_name%>_list"].append(render("<%=plural_name%>/magic_list_item", :<%=singular_name%> => @<%=singular_name%>))
         @<%=singular_name%> = <%=class_name%>.new
