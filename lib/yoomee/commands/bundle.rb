3ccc4a453868a96705e4f6d7e82bd47e4858ece0
Matt Atkins <matt@yoomee.com>
2010-11-12 13:18:05 +0000

    Edit and reveal bundle.

--- lib/yoomee/commands/bundle.rb
+++ lib/yoomee/commands/bundle.rb
@@ -1,21 +1,27 @@
 module Yoomee::Command
   class Bundle < Base
+    
+    def edit
+      shell("mate #{escaped_bundle_path}/")
+    end
 
     def install
-      bundles_dir = "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles"
+      bundles_dir = bundle_path.chomp("/Yoomee.tmbundle")
       Dir.mkdir(bundles_dir) unless File.directory?(bundles_dir)
-      if File.directory?(File.join(bundles_dir, "Yoomee.tmbundle"))
+      if File.directory?(bundle_path)
         display("Bundle already installed, use ym bundle:update.")
       else
-        git("clone", "textmate/yoomee", File.join(bundles_dir, "Yoomee.tmbundle"))
+        git("clone", "textmate/yoomee", bundle_path)
         shell("osascript -e 'tell app \"TextMate\" to reload bundles'")
         display("Bundle installed successfully.")
       end
     end
     
+    def reveal
+      shell("open -a Finder #{escaped_bundle_path.chomp("/Yoomee.tmbundle")}")
+    end
+    
     def update
-      bundle_path = "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/Yoomee.tmbundle"
-      puts bundle_path
       if File.directory?(bundle_path)
         display("Moved existing version of bundle to trash.") if trash(bundle_path)
         install
@@ -23,6 +29,15 @@ module Yoomee::Command
         display("Bundle not yet installed, installing now...")
         install
       end
+    end
+    
+    private
+    def bundle_path
+      "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/Yoomee.tmbundle"
+    end
+    
+    def escaped_bundle_path
+      bundle_path.gsub(/ /,"\\ ")
     end
 
   end
