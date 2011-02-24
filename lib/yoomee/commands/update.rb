f1852bb54ae08453e9a8ead8665f3ff71c2bf6e1
Matt Atkins <matt@yoomee.com>
2011-02-24 09:44:36 +0000

    Detect migrations in ym update. Added ym st

--- lib/yoomee/commands/update.rb
+++ lib/yoomee/commands/update.rb
@@ -6,6 +6,11 @@ module Yoomee::Command
       repos = str.split(/updating/)
       updated_repos = repos.reject{|repo| repo.match(/Already up-to-date/)}.join("updating")
       puts updated_repos.empty? ? "Everything is already up-to-date." : updated_repos
+      migrations = str.scan(/\w*\/?migrate[^\s]*/).uniq
+      if !migrations.empty?
+        puts "\n\nAdded #{migrations.size} migration#{'s' if migrations.size > 1}:\n + #{migrations.join("\n + ")}"
+        system("rake db:migrate") if confirm("Run rake db:migrate now? (y/n)")
+      end
     end
   end
 end
