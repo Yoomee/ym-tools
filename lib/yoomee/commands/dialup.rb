a773cfe6793ec8d3a3295c73457a793b045d147b
Matt Atkins <matt@yoomee.com>
2011-11-10 11:38:06 +0000

    ym dialup

---
+++ lib/yoomee/commands/dialup.rb
@@ -0,0 +1,8 @@
+module Yoomee::Command
+  class Dialup < Base
+    def index
+      mp3 = File.dirname(__FILE__) + '/../../support/dialup.mp3'
+      system("/usr/bin/afplay #{mp3}")
+    end
+  end
+end
