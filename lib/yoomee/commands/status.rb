0ff66dc167fc30e024e9c6bc074332ba5def72c3
Matt Atkins <matt@yoomee.com>
2012-03-29 10:57:17 +0100

    Improved ym status. Removed unused alias commands.

--- lib/yoomee/commands/status.rb
+++ lib/yoomee/commands/status.rb
@@ -1,17 +1,21 @@
 module Yoomee::Command
   class Status < Base
     def index
-      puts "Getting status of project and all local gems..."
-      statuses = [[
-        "root",
-        IO.popen("git status").read
-      ]]
+      @clean = []
+      puts_status("root",IO.popen("git status").read)
       Dir["vendor/gems/**"].each do |gem_path|
-        statuses << [gem_path, IO.popen("cd #{gem_path} && git status").read]
+        puts_status(gem_path,IO.popen("cd #{gem_path} && git status").read)
       end
-      statuses.each do |status|
-        puts status[0] unless status[0] == "root"
-        puts status[1].to_s
+      puts "nothing to commit: #{@clean.join(', ')}"
+    end
+    
+    private
+    def puts_status(dir,status)
+      if status =~ /working directory clean/m
+        @clean << dir.sub(/vendor\/gems\//,'')
+      else
+        puts dir unless dir == "root"
+        puts status
         puts "\n"
       end
     end
