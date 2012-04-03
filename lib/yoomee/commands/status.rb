97e0aae289bfa1111722f37220588fc8afff78e9
Matt Atkins <matt@yoomee.com>
2012-04-03 12:19:11 +0100

    Show repos that are ahead.

--- lib/yoomee/commands/status.rb
+++ lib/yoomee/commands/status.rb
@@ -6,12 +6,12 @@ module Yoomee::Command
       Dir["vendor/gems/**"].each do |gem_path|
         puts_status(gem_path,IO.popen("cd #{gem_path} && git status").read)
       end
-      puts "nothing to commit: #{@clean.join(', ')}"
+      puts "clean: #{@clean.join(', ')}"
     end
     
     private
     def puts_status(dir,status)
-      if status =~ /working directory clean/m
+      if !(status =~ /ahead/m) && status =~ /working directory clean/m
         @clean << dir.sub(/vendor\/gems\//,'')
       else
         puts dir unless dir == "root"
